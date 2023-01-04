f66f30b1b468ce68beed3ccd2a42d28cf53057ab
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-01-04 08:41:35 +0100

    Front

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/MainWindow.fxml"));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/View/MainWindow.fxml"));
         Parent root = loader.load();
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
