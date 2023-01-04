ce963eae30f89cccfe1b5c26dd85680668a5681e
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-01-04 16:42:27 +0100

    Update MovieDAO_DB.java

--- src/DAL/db/MovieDAO_DB.java
+++ src/DAL/db/MovieDAO_DB.java
@@ -26,8 +26,8 @@ public class MovieDAO_DB implements IMovieDAO {
             while (rs.next()) {
                 int id = rs.getInt("MovieID");
                 String title = rs.getString("Title");
-                Float IMDBRating = rs.getFloat("IMDB"); //todo Check if correct
-                Float PersonalRating = rs.getFloat("Personal"); //TODO check if correct
+                float IMDBRating = rs.getFloat("IMDBRating"); //todo Check if correct
+                float PersonalRating = rs.getFloat("PersonalRating"); //TODO check if correct
                 String filePath = rs.getString("FilePath"); // TODO CHECK IF CORRECT
 
                 Movie movie = new Movie(id,title,IMDBRating,PersonalRating,filePath);
@@ -41,13 +41,13 @@ public class MovieDAO_DB implements IMovieDAO {
     @Override
     public Movie createMovie(String title, String year, float imdbRating, float personalRating, String filePath) throws Exception {
 
-        String sql = "INSERT INTO movie (Id,Title,Year,IMDBRating,PersonalRating,FilePath) VALUES (?,?,?,?,?);";
+        String sql = "INSERT INTO movie (Id,Title,IMDBRating,PersonalRating,FilePath) VALUES (?,?,?,?,?);";
 
 
         try (Connection conn = databaseConnector.getConnection()) {
             PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
-            stmt.setString(1, title);
+            stmt.setString(2, title);
             stmt.setFloat(3, imdbRating);
             stmt.setFloat(4, personalRating);
             stmt.setString(5, filePath);
