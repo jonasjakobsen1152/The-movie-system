5ff15e17e3ef7e409a02bc3210e9689cb7101d2c
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-01-04 09:30:07 +0100

    Update MovieDataInputs.java

--- src/GUI/Controller/MovieDataInputs.java
+++ src/GUI/Controller/MovieDataInputs.java
@@ -1,5 +1,15 @@
 package GUI.Controller;
 
+import javafx.event.ActionEvent;
+
 public class MovieDataInputs {
 
+    public void handleChooseFile(ActionEvent actionEvent) {
+    }
+
+    public void handleCreateMovie(ActionEvent actionEvent) {
+    }
+
+    public void handleCreationCancel(ActionEvent actionEvent) {
+    }
 }
