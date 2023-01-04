07a3cb64fea8b2e7cd2601efe3f121d204dba612
Rater1 <pjuske.10@live.dk>
2023-01-04 09:57:59 +0100

    chooseFile

--- src/GUI/Controller/MovieDataInputs.java
+++ src/GUI/Controller/MovieDataInputs.java
@@ -1,7 +1,12 @@
 package GUI.Controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class MovieDataInputs {
 
@@ -10,8 +15,23 @@ public class MovieDataInputs {
     public TextField txtPersonalRating;
     public TextField txtYear;
     public TextField txtFilePath;
+    public Button btnCreateMovie;
+    public Button txtChooseFile;
+    public Button btnCancel;
 
     public void handleChooseFile(ActionEvent actionEvent) {
+        String fileName;
+        Stage stage = new Stage();
+        FileChooser fc = new FileChooser();
+        File file = fc.showOpenDialog(stage);
+        if (file != null) {
+
+            fileName = file.toURI().toString();
+            fileName=fileName.substring(6);
+            String newFile = fileName;
+            fileName = newFile.replace("%20"," ");
+            txtFilePath.setText(fileName);
+        }
     }
 
     public void handleCreateMovie(ActionEvent actionEvent) {
