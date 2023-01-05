01e38e6a29ab8ed00bc9fe86b759609cc1e076ed
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-01-05 11:17:36 +0100

    Update MovieDAO_DB.java

--- src/DAL/db/MovieDAO_DB.java
+++ src/DAL/db/MovieDAO_DB.java
@@ -35,7 +35,8 @@ public class MovieDAO_DB implements IMovieDAO {
             }
             return allMovies;
 
-        }        catch (SQLException ex)
+        }
+        catch (SQLException ex)
         {
             ex.printStackTrace();
             throw new Exception("Could not get Songs from database", ex);
@@ -47,16 +48,16 @@ public class MovieDAO_DB implements IMovieDAO {
     @Override
     public Movie createMovie(String title, float imdbRating, float personalRating, String filePath) throws Exception {
 
-        String sql = "INSERT INTO movie (Id,Title,IMDBRating,PersonalRating,FilePath) VALUES (?,?,?,?,?);";
+        String sql = "INSERT INTO movie (Title,IMDBRating,PersonalRating,FilePath) VALUES (?,?,?,?);";
 
 
         try (Connection conn = databaseConnector.getConnection()) {
             PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
-            stmt.setString(2, title);
-            stmt.setFloat(3, imdbRating);
-            stmt.setFloat(4, personalRating);
-            stmt.setString(5, filePath);
+            stmt.setString(1, title);
+            stmt.setFloat(2, imdbRating);
+            stmt.setFloat(3, personalRating);
+            stmt.setString(4, filePath);
 
             // Run the specified SQL statement
             stmt.executeUpdate();
