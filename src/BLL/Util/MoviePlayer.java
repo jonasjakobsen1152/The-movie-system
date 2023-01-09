2115879a67080af444a4c2e86c7fa0324ce8846f
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2023-01-09 08:59:45 +0100

    mediaplayer

---
+++ src/BLL/Util/MoviePlayer.java
@@ -0,0 +1,15 @@
+package BLL.Util;
+
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
+
+public class MoviePlayer {
+
+    File file = new File("")
+
+
+
+
+}
